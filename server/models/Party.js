0a0de678af17e0a7f29a8d6aba50423b372f0729
Michael Maffattone <mikeymaff@gmail.com>
2014-09-16 09:06:32 -0400

    add party references to each song

--- server/models/Party.js
+++ server/models/Party.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose'),
 
 
 var partySchema = mongoose.Schema({
-	//_id: Number,
 	name: {type:String, required:'{PATH} is required!'},
 	dj: {type:String, required:'{PATH} is required!'},
 	location: {type:String, required:'{PATH} is required!'},
@@ -15,7 +14,12 @@ var songSchema = mongoose.Schema({
 	//_party: Number,
 	name: {type:String, required:'{PATH} is required!'},
 	artist: {type:String, required:'{PATH} is required!'},
-	voteCount: {type:Number},
+	_parties: [
+		{
+			partyId:{type:Schema.Types.ObjectId, ref: 'Party'},
+			voteCount:Number
+		}
+	]
 });
 
 var Party = mongoose.model('Party', partySchema);
@@ -27,9 +31,9 @@ function createDefaultParties() {
 		if(collection.length === 0) {
 			Party.create({name: 'New Years Eve', dj: 'Dada Life', location: 'Pacha'}, function(err, myParty) {
 				var songs = [
-					new Song({name:"Gecko", artist:"Oliver Heldens", voteCount:1}),
-					new Song({name:"You Should Know", artist:"Jack Beats", voteCount:1}),
-					new Song({name:"Problem", artist:"Arriana Grande", voteCount:1})
+					new Song({name:"Gecko", artist:"Oliver Heldens", _parties:[{partyId: myParty, voteCount:1}]}),
+					new Song({name:"You Should Know", artist:"Jack Beats", _parties:[{partyId: myParty, voteCount:1}]}),
+					new Song({name:"Problem", artist:"Arriana Grande", _parties:[{partyId: myParty, voteCount:1}]})
 				]
 
 				for(i=0; i<songs.length; i++) {
@@ -42,8 +46,8 @@ function createDefaultParties() {
 
 			Party.create({name: 'Electric Zoo', dj: 'Steve Aoki', location: 'Randalls Island'}, function(err, myParty) {
 				var songs = [
-					new Song({name:"Atmosphere", artist:"Kaskade", voteCount:1}),
-					new Song({name:"Thinking About You", artist:"Calvin Harris", voteCount:1})
+					new Song({name:"Atmosphere", artist:"Kaskade", _parties:[{partyId: myParty, voteCount:1}]}),
+					new Song({name:"Thinking About You", artist:"Calvin Harris", _parties:[{partyId: myParty, voteCount:1}]})
 				]
 
 				for(i=0; i<songs.length; i++) {
@@ -56,10 +60,10 @@ function createDefaultParties() {
 
 			Party.create({name: 'Boys and Girls Club', dj: 'Wolfgang Gartner', location: 'Webster Hall'}, function(err, myParty) {
 				var songs = [
-					new Song({name:"Rift", artist:"Dirty South", voteCount:1}),
-					new Song({name:"Codec", artist:"Zedd", voteCount:1}),
-					new Song({name:"I Know The Truth", artist:"Pretty Lights", voteCount:1}),
-					new Song({name:"Latch", artist:"Disclosure", voteCount:1})
+					new Song({name:"Rift", artist:"Dirty South", _parties:[{partyId: myParty, voteCount:1}]}),
+					new Song({name:"Codec", artist:"Zedd", _parties:[{partyId: myParty, voteCount:1}]}),
+					new Song({name:"I Know The Truth", artist:"Pretty Lights", _parties:[{partyId: myParty, voteCount:1}]}),
+					new Song({name:"Latch", artist:"Disclosure", _parties:[{partyId: myParty, voteCount:1}]})
 				]
 
 				for(i=0; i<songs.length; i++) {
